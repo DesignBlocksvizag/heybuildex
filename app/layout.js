116a29d2f28b2fa7b045612d8df8ddeae831d159
murali1466 <muralikrishnatangella1466@gmail.com>
2025-07-02 16:21:11 +0530

    add facebook pixel

--- app/layout.js
+++ app/layout.js
@@ -56,21 +56,49 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="ReUglBolff0mjryhaGopONsOrcRvls2fENNz0kBVVuE"
         />
+        {/* Facebook Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '722479297445145');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* Facebook Pixel NoScript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=722479297445145&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
-      <body>{children}
-        {/* Google Tag Manager */}
-           <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=AW-17164106962"
-      />
-      <Script id="google-ads" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-17164106962');
-        `}
-      </Script>
+      <body>
+        {children}
+
+        {/* Google Ads */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17164106962"
+        />
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17164106962');
+          `}
+        </Script>
+
+        {/* Google Analytics */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-ZLFD84G5G3"
@@ -83,23 +111,6 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-ZLFD84G5G3');
           `}
         </Script>
-           <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '9976954195721150');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-           <Script id="gtag-conversion-inline" strategy="afterInteractive">
-                {`gtag('event', 'conversion', {'send_to': 'AW-17164106962/ndwUCLmv39YaENL5vfg_'});`}
-          </Script>
       </body>
     </html>
   );
