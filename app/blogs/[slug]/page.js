a68b19c2b02e3373811fe76fe67cb1646a9fcb47
murali1466 <muralikrishnatangella1466@gmail.com>
2025-08-12 09:44:00 +0530

    add title metadata

--- app/blogs/[slug]/page.js
+++ app/blogs/[slug]/page.js
@@ -2,6 +2,28 @@ import Footer from "@/src/components/Footer";
 import Header from "@/src/components/Header";
 import SteelStructuresBlog from "@/src/content/Blogs";
 import { notFound } from "next/navigation";
+export async function generateMetadata({ params }) {
+  const { slug } = await params;
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE}/api/blogs/read?slug=${slug}`,
+      { cache: "no-store" }
+    );
+
+    if (!res.ok) {
+      return { title: "Blog Not Found" };
+    }
+
+    const blogData = await res.json();
+    return {
+      title: blogData.blog?.heading || "Untitled Blog",
+    };
+  } catch (error) {
+    return { title: "Error Loading Blog" };
+  }
+}
+
 
 export default async function BlogPageView({ params }) {
   const resolvedParams = await params;
